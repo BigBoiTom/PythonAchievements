419645eac556e3f8fcc13f5e00b956f9f74bf637
BigBoiTom <BigBoiTom@users.noreply.github.com>
2020-09-18 14:30:29 +0200

    PLayer statistics added

---
+++ PYTB1L2PlayerStatistics/PlayerStatistics.py
@@ -0,0 +1,21 @@
+name = "Name: Carl"
+element = "Element: Darkness"
+lives = "Lives: Infinite"
+type = "Type: God"
+level = "Level: 1000"
+size = "Size: Galaxy wide"
+speed = "Speed: Multiple Lightyears"
+clan = "Clan: Gods of the universe"
+braincapacity = "Brain Capacity: -2.2"
+affiliation = "Affiliation: Evil"
+
+print(name)
+print(element)
+print(lives)
+print(type)
+print(level)
+print(size)
+print(speed)
+print(clan)
+print(braincapacity)
+print(affiliation)
